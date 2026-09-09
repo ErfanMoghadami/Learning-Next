ff839b2036286c7d58df575f500819ee9829779d
ErfanMoghadami <erfanmoqadami@gmail.com>
2026-09-09 20:52:57 +0330

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import "@/app/ui/global.css";
+import { interFont } from "@/app/ui/fonts";
+
+export default function RootLayout({children,}: { children: React.ReactNode;}) {
+  return (
+    <html lang="en">
+      <body className={`${interFont.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
