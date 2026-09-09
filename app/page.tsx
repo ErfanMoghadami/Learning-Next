6814c46a2dc00799704c939dd8ceb1e8227e5541
ErfanMoghadami <erfanmoqadami@gmail.com>
2026-09-09 21:23:30 +0330

    fix: update fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
 
-import { lusitanaFont } from "@/app/ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 export default function Page() {
   return (
     <main className="flex from-lime-700n-h-screen flex-col p-6">
@@ -14,7 +14,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
-            className={`${lusitanaFont.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           ></p>
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
