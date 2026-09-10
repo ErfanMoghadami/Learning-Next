e1b9fa20fda38035d7823c91ec1cfb3f5cc0230b
ErfanMoghadami <erfanmoqadami@gmail.com>
2026-09-10 18:52:01 +0330

    Yeah Im Slow

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
-
 import { lusitana } from "@/app/ui/fonts";
+
+
+
+
+
+
+
+
 export default function Page() {
   return (
     <main className="flex from-lime-700n-h-screen flex-col p-6">
