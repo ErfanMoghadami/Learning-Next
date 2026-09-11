c85dcda006b90f0f6f912940a70b2eabb673601e
ErfanMoghadami <erfanmoqadami@gmail.com>
2026-09-11 19:39:54 +0330

    its Breaking My HeartAndBrain

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,6 +5,8 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
+import { usePathname, useSearchParams } from 'next/navigation';
+
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 10
 
